9ecb3343133637c66ff57b20c7256c31ae753d0d
sudee404 <sudaise18@gmail.com>
2024-12-06 02:28:35 +0300

    update admin.py

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -8,7 +8,6 @@ class MyUserAdmin(UserAdmin):
         (None, {'fields': ('email', 'password')}),
         ('Personal Info', {'fields': ('first_name', 'last_name', 'username', 'account_type', 'recovery_email')}),
         ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser', 'groups', 'user_permissions')}),
-        ('Important dates', {'fields': ('last_login', 'created_at', 'updated_at')}),
     )
     add_fieldsets = (
         (None, {
