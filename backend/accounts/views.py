c2e57f376bbfd257db6f3f53b30f19f5320d263e
sudee404 <sudaise18@gmail.com>
2024-12-06 17:43:28 +0300

    create 1 file and update 3 files

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -61,4 +61,11 @@ class UserViewSet(viewsets.ModelViewSet):
     def list(self, request):
         user = request.user
         serializer = UserSerializer(user,context={'request': request})
-        return Response({'data':serializer.data},status=status.HTTP_200_OK)
+        return Response({'user':serializer.data},status=status.HTTP_200_OK)
+    
+    def create(self, request, *args, **kwargs):
+        user = request.user
+        serializer = self.get_serializer(user, data=request.data, partial=True)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_200_OK)
--- backend/core/settings.py
+++ backend/core/settings.py
@@ -128,7 +128,7 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'AUTH_HEADER_TYPES': ('BEARER',),
+    'AUTH_HEADER_TYPES': ('Bearer',),
     'ACCESS_TOKEN_LIFETIME': timedelta(days=7),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
     'ALGORITHM': 'HS256',
